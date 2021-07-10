f3d37f85d2a7f44e0f67b2aef97dbf38f7307c48
tai.yamazaki <yamayamayamataitaitai@gmail.com>
2021-07-10 18:19:20 +0900

    Add NewServlet.java

---
+++ src/filters/EncodingFilter.java
@@ -0,0 +1,55 @@
+package filters;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+
+/**
+ * Servlet Filter implementation class EncodingFilter
+ */
+@WebFilter("/encoding")
+public class EncodingFilter implements Filter {
+
+    /**
+     * Default constructor.
+     */
+    public EncodingFilter() {
+
+    }
+
+    /**
+     * @see Filter#destroy()
+     */
+    @Override
+    public void destroy() {
+        ;
+    }
+
+    /**
+     * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+     */
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+
+        request.setCharacterEncoding("UTF-8");
+        response.setCharacterEncoding("UTF-8");
+
+        chain.doFilter(request, response);
+    }
+
+    /**
+     * @see Filter#init(FilterConfig)
+     */
+    @Override
+    public void init(FilterConfig fConfig) throws ServletException {
+
+    }
+
+}
